66d78827d2c730bc8fb66dfa53e69f4ce10f9643
allthatilk <imogenkutz@hotmail.com>
2017-03-09 23:09:51 +0000

    Revert "set env var via config:set"
    This reverts commit 114c4f5dd040f489f05f94b4492272eb320ee643.

--- config.rb
+++ config.rb
@@ -2,13 +2,11 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 Sinatra::Base.configure :development do
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_development")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_development")
 end
 
 Sinatra::Base.configure :test do
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_test")
 end
 
 Sinatra::Base.configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
