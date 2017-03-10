e0aed7dd8a0a76e009c731b66fde8d92e4e43f91
allthatilk <imogenkutz@hotmail.com>
2017-03-10 16:51:36 +0000

    Refactored datamapper_config

--- models/link.rb
+++ models/link.rb
@@ -1,6 +1,3 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
 class Link
   include DataMapper::Resource
 
@@ -19,9 +16,3 @@ class Tag
 
   has n, :link, :through => Resource
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
